8cc52048956ca845be1fd23746362b166ad69e6d
lokes <lokes@LAPTOP-J2B9HTQV>
2022-01-13 17:09:35 +0530

    Add project files.

---
+++ Project/Data/ProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Project.Controllers;
+
+namespace Project.Data
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext (DbContextOptions<ProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Project.Controllers.Employee> Employee { get; set; }
+    }
+}
